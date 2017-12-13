10c75ef33c5afa6c5a4ebdff74b268ee080f460d
titi_ <titi_@192.168.1.54>
2017-12-13 12:30:21 +0100

    ejercicio 3 Mario Fernandez Diez

---
+++ src/mario_fernandez_clase_simple.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author titi_
+ */
+public class mario_fernandez_clase_simple {
+    private String mario;
+    private String fernandez;
+}
